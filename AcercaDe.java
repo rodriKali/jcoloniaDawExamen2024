225e8974a176475e858dbf39483a228496eba62b
Rodrigo Ortega <rodrigo.ortpar.1@educa.jcyl.es>
2024-05-03 13:04:48 +0200

    Acerca de informacion

---
+++ AcercaDe.java
@@ -0,0 +1,21 @@
+package jcolonia.daw2023.quiniela;
+
+/**
+ * Clase para información acerca del programa EJERCICIO 4
+ * Esta clase muestra información sobre el desarrollador, la fecha y la versión del programa.
+ * 
+ * @author Rodrigo Ortega
+ * @version 1.0
+ */
+public class AcercaDe {
+    
+    /**
+     * Muestra información acerca del programa.
+     */
+    public static void mostrarInformacion() {
+        System.out.println("--- Acerca de ---");
+        System.out.println("Desarrollador Examen: Rodrigo Ortega");
+        System.out.println("Fecha: 2024");
+        System.out.println("Versión: 1.0");
+    }
+}
